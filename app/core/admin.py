c12d67cef2c1b08159fdeed6710364c6e50442e1
Diaga <diagaaos@gmail.com>
2020-12-06 20:43:48 +0500

    feat(admin): register models to django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.register(models.Keyword)
+admin.register(models.Degree)
+admin.register(models.University)
+admin.register(models.User)
+admin.register(models.Mentor)
+admin.register(models.Student)
+admin.register(models.Question)
+admin.register(models.Answer)
+admin.register(models.Comment)
+admin.register(models.Upvote)
+admin.register(models.FeedbackForm)
+admin.register(models.PairSession)
